521c0e1659c5459dbcd575e73c21c054af238882
jassuwu <karthickprnv@gmail.com>
2024-05-26 08:37:11 +0530

    scraper: persistance changes for proper structure

--- scraper/persist.go
+++ scraper/persist.go
@@ -15,12 +15,13 @@ func initJSONAndStartVisiting(startingLink string, visitFn func(string) error) {
 		fmt.Println("JSON file couldn't be created:", err)
 		return
 	}
-	_, err = file.WriteString("{\n\n}")
+	_, err = file.WriteString("{")
 	if err != nil {
 		fmt.Println("JSON file couldn't be initialized:", err)
 	}
 	file.Close()
 
+	fmt.Println("Opened the curly")
 	visitFn(startingLink)
 
 	// Close the JSON object
@@ -29,7 +30,7 @@ func initJSONAndStartVisiting(startingLink string, visitFn func(string) error) {
 		fmt.Println("JSON file couldn't be opened:", err)
 		return
 	}
-	_, err = file.WriteString("\n}")
+	_, err = file.WriteString("}")
 	if err != nil {
 		fmt.Println("JSON file couldn't be closed properly:", err)
 	}
@@ -37,7 +38,7 @@ func initJSONAndStartVisiting(startingLink string, visitFn func(string) error) {
 }
 
 func appendToJSON(pageDocument PageDocument) {
-	file, err := os.OpenFile(PSGTECH_JSON_FILE_PATH, os.O_RDWR, 0644)
+	file, err := os.OpenFile(PSGTECH_JSON_FILE_PATH, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println("JSON file couldn't be opened:", err)
 		return
@@ -50,12 +51,16 @@ func appendToJSON(pageDocument PageDocument) {
 		return
 	}
 
-	if fileInfo.Size() > 2 { // Check if the file already contains entries
+	fmt.Println(fileInfo.Size())
+	if fileInfo.Size() > 1 { // Check if the file already contains entries
 		// Move back two bytes to overwrite the closing }
-		file.Seek(fileInfo.Size()-2, 0)
-		file.WriteString(",\n")
+		file.Seek(fileInfo.Size()-1, 0)
+		file.WriteString(",")
+	} else {
+		fmt.Println("didn't insert a comma now ")
 	}
 
+	fmt.Println(fileInfo.Size())
 	entry := fmt.Sprintf(`"%s":`, pageDocument.Url)
 	_, err = file.WriteString(entry)
 	if err != nil {
@@ -63,16 +68,11 @@ func appendToJSON(pageDocument PageDocument) {
 		return
 	}
 
+	fmt.Println(fileInfo.Size())
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(pageDocument)
 	if err != nil {
 		fmt.Println("Couldn't encode data to JSON:", err)
 		return
-	}
-
-	// Close the JSON object
-	_, err = file.WriteString("\n}")
-	if err != nil {
-		fmt.Println("JSON file couldn't be closed properly:", err)
 	}
 }
